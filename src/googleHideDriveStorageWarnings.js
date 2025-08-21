42c8c4e6d8720c405ee3395e02fdaed6e3d64a7d
Gavin Nicol <ngavin@umich.edu>
2025-08-21 08:26:53 -0500

    fix

--- src/googleHideDriveStorageWarnings.js
+++ src/googleHideDriveStorageWarnings.js
@@ -1,1 +1,6 @@
-setInterval(() => document.querySelector('.docs-recommendation-banner-container').hidden = true, 1000)
+setInterval(() => {
+	const banner = document.querySelector('#docs-recommendation-banner-container');
+	if (banner) {
+		banner.hidden = true;
+	}
+}, 1000);
