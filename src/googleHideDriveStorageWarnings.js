952fe37b14f5d4bb7fab5c78df80d59eb9b3cdb4
Gavin Nicol <ngavin@umich.edu>
2025-09-10 14:36:13 -0500

    efficiency

--- src/googleHideDriveStorageWarnings.js
+++ src/googleHideDriveStorageWarnings.js
@@ -1,13 +1,8 @@
-setInterval(() => {
-	const banner = document.querySelector('#docs-recommendation-banner-container');
-	if (banner) {
-		banner.style.display = 'none';
-	}
-}, 1000);
-
-setInterval(() => {
+const intervalId = setInterval(() => {
+	// const banner = document.querySelector('#docs-recommendation-banner-container');
 	const banner = document.querySelector('#docs-banner-container');
 	if (banner) {
 		banner.style.display = 'none';
+		clearInterval(intervalId); // stop checking once hidden
 	}
 }, 1000);
